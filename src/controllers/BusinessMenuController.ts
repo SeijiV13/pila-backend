d0ad6d9965a9629b71f4260294befc1d8c1f0270
Seiji Villafranca <seijiv13@outlook.com>
2019-10-28 06:31:13 +0000

    Merged PR 43: added business menu controlle
    added business menu controlle

---
+++ src/controllers/BusinessMenuController.ts
@@ -0,0 +1,78 @@
+import { validate } from 'class-validator';
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { Businessmenu } from '../entity/Businessmenu';
+
+class BusinessMenuController {
+  public static getAllBusinessMenu = async (req: Request, res: Response) => {
+    const businessMenuRepository = getRepository(Businessmenu);
+    const allbusinesMenu = await businessMenuRepository.find({
+      select: [
+        'id',
+        'businessId',
+        'categoryId',
+        'name',
+        'description',
+        'price',
+        'isDeleted',
+        'dateCreated',
+        'dateModified',
+      ],
+    });
+
+    res.send(allbusinesMenu).status(200);
+  };
+
+  public static getAllBusinessMenuOfOneBusiness = async (req: Request, res: Response) => {
+    const { businessId } = req.body;
+    const businessMenuRepository = getRepository(Businessmenu);
+
+    const businessMenu = await businessMenuRepository.find({
+      select: [
+        'id',
+        'businessId',
+        'categoryId',
+        'name',
+        'description',
+        'price',
+        'isDeleted',
+        'dateCreated',
+        'dateModified',
+      ],
+      where: { businessId },
+    });
+
+    res.send(businessMenu).status(200);
+  };
+
+  public static createBusinessMenu = async (req: Request, res: Response) => {
+    const { businessId, categoryId, name, description, price } = req.body;
+    const businessMenuRepository = getRepository(Businessmenu);
+    const businessMenu = new Businessmenu();
+    businessMenu.businessId = businessId;
+    businessMenu.name = name;
+    businessMenu.categoryId = categoryId;
+    businessMenu.description = description;
+    businessMenu.price = price;
+    businessMenu.isDeleted = 0;
+    businessMenu.dateCreated = new Date();
+    businessMenu.dateModified = new Date();
+    // Validade if the parameters are ok
+    const errors = await validate(businessMenu);
+    if (errors.length > 0) {
+      res.status(400).send(errors);
+      return;
+    }
+
+    try {
+      await businessMenuRepository.save(businessMenu);
+    } catch (e) {
+      // writeFile for logs
+      console.log(e);
+      res.status(409).send(e);
+      return;
+    }
+  };
+}
+
+export default BusinessMenuController;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express';
 import auth from './auth';
 import business from './business';
+import businessmenu from './business-menu';
 import category from './category';
 import profile from './profile';
 import user from './user';
@@ -11,5 +12,6 @@ routes.use('/user', user);
 routes.use('/business', business);
 routes.use('/profile', profile);
 routes.use('/category', category);
+routes.use('/business-menu', businessmenu);
 
 export default routes;
