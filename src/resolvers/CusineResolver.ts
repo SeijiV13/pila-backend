769ce63f3c6dfaed1706a02c210b62033e21cf02
Jez Reel Maghuyop <jezreelmaghuyop@outlook.com>
2020-08-23 05:27:02 +0000

    Merged PR 89: add cuisine endpoint
    add cuisine endpoint
    add thumbnail property to cuisine

--- src/resolvers/CusineResolver.ts
+++ src/resolvers/CusineResolver.ts
@@ -6,21 +6,46 @@ export class CuisineResolver {
   @Query(() => [Cuisine])
   public async getCuisines() {
     const restaurantTypes: Cuisine[] = [
-      { description: 'American' },
-      { description: 'Chinese' },
-      { description: 'Filipino' },
-      { description: 'French' },
-      { description: 'Greek' },
-      { description: 'Indian' },
-      { description: 'Italian' },
-      { description: 'Japanese' },
-      { description: 'Korean' },
-      { description: 'Mexican' },
-      { description: 'Middle Eastern' },
-      { description: 'Singaporean' },
-      { description: 'Soul Food' },
-      { description: 'Thai' },
-      { description: 'Vegetarian' },
+      {
+        description: 'American',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/american-foods.png',
+      },
+      {
+        description: 'Chinese',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/chinese.png',
+      },
+      {
+        description: 'Filipino',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/filipino.png',
+      },
+      {
+        description: 'Indian',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/indian.png',
+      },
+      {
+        description: 'Japanese',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/japanese.png',
+      },
+      {
+        description: 'Mexican',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/american-foods.png',
+      },
+      {
+        description: 'Singaporean',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/american-foods.png',
+      },
+      {
+        description: 'Thai',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/thai.png',
+      },
+      {
+        description: 'Italian',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/italian.png',
+      },
+      {
+        description: 'Spanish',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/cuisine/spanish.png',
+      },
     ];
 
     return restaurantTypes;
