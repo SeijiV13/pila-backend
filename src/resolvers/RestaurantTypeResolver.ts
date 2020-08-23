54e9390c3a04a6d377f6506918a145620b5456a1
Jez Reel Maghuyop <jezreelmaghuyop@outlook.com>
2020-08-23 06:38:55 +0000

    Merged PR 90: add thumbnail to resto type
    add thumbnail to resto type

--- src/resolvers/RestaurantTypeResolver.ts
+++ src/resolvers/RestaurantTypeResolver.ts
@@ -6,15 +6,48 @@ export class RestaurantTypeResolver {
   @Query(() => [RestaurantType])
   public async getRestaurantTypes() {
     const restaurantTypes: RestaurantType[] = [
-      { description: 'Buffet' },
-      { description: 'Cafe' },
-      { description: 'Casual Dining' },
-      { description: 'Contemporary Casual' },
-      { description: 'Ethnic' },
-      { description: 'Family Style' },
-      { description: 'Fast Casual' },
-      { description: 'Fast Food' },
-      { description: 'Fine Dining' },
+      {
+        description: 'Buffet',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/buffet.png',
+      },
+      {
+        description: 'Cafe',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/cafe.jpg',
+      },
+      {
+        description: 'Casual Dining',
+        thumbnail:
+          'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/casual-dining.jpg',
+      },
+      {
+        description: 'Contemporary Casual',
+        thumbnail:
+          'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/contemporary-casual.jpg',
+      },
+      {
+        description: 'Ethnic',
+        thumbnail: 'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/cafe.jpg',
+      },
+      {
+        description: 'Family Style',
+        thumbnail:
+          'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/family-style.jpg',
+      },
+      {
+        description: 'Fast Casual',
+        thumbnail:
+          'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/fast-casual.jpg',
+      },
+      {
+        description: 'Fast Food',
+        thumbnail:
+          'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/fast-food.jpg',
+      },
+      {
+        description: 'Fine Dining',
+        thumbnail:
+          'https://pilastorage.blob.core.windows.net/images/resto-type-mockup/fine-dining2.jpg',
+      },
     ];
 
     return restaurantTypes;
